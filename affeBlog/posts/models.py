eeeb054c0305d83e98d01c50fd8e0c3284be74d1
affe <affeisme@gmail.com>
2018-11-25 18:25:01 -0800

    reverse clear

--- affeBlog/posts/models.py
+++ affeBlog/posts/models.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 from django.conf import settings
 # TODO slugify and reverse not found
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.db.models.signals import pre_save
 from django.utils import timezone
 from django.utils.text import slugify
