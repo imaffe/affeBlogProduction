1ef6141c3991f288961651b1a3d9fb25a2f8f56b
affe <affeisme@gmail.com>
2018-11-25 20:07:46 -0800

    typo

--- affeBlog/affeBlog/urls.py
+++ affeBlog/affeBlog/urls.py
@@ -18,14 +18,13 @@ from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
 
-from posts import views
 urlpatterns = [
     path(r'^admin/', admin.site.urls),
     path(r'^posts/', include("posts.urls"), namespace='posts'),  # appname.views.function_name
 ]
 
 
-if settings.DEBUG:
+if settings.DEBUG:r
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     #urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
