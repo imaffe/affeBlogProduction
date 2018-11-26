6c3c153cd9c567efea350498b0fbd35ebd91c390
affe <affeisme@gmail.com>
2018-11-25 20:43:16 -0800

    typo

--- affeBlog/posts/urls.py
+++ affeBlog/posts/urls.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
-from django.urls import  path
+from django.urls import path, re_path
 
 from posts import views
 
 
 urlpatterns = [
-    path(r'^$', views.post_list, name='list'),
-    path(r'^create/$', views.post_create),
-    path(r'^(?P<slug>[\w-]+)/$', views.PostDetailView.as_view(), name='detail'),
-    path(r'^(?P<slug>[\w-]+)/edit/$', views.post_update, name='update'),
-    path(r'^(?P<slug>[\w-]+)/delete/$', views.post_delete),
+    re_path(r'^$', views.post_list, name='list'),
+    re_path(r'^create/$', views.post_create),
+    re_path(r'^(?P<slug>[\w-]+)/$', views.PostDetailView.as_view(), name='detail'),
+    re_path(r'^(?P<slug>[\w-]+)/edit/$', views.post_update, name='update'),
+    re_path(r'^(?P<slug>[\w-]+)/delete/$', views.post_delete),
     #url(r'^posts/$', "<appname>.views.<function_name>"),
 ]
