4862ba1e57cba68062fe6d312dfe92c5ee8a570e
affe <affeisme@gmail.com>
2018-11-25 20:44:00 -0800

    typo

--- affeBlog/affeBlog/urls.py
+++ affeBlog/affeBlog/urls.py
@@ -14,13 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path(r'^admin/', admin.site.urls),
-    path(r'^posts/', include(("posts.urls", "posts"), namespace='posts')),  # appname.views.function_name
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'^posts/', include(("posts.urls", "posts"), namespace='posts')),  # appname.views.function_name
 ]
 
 
